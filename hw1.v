7b29f9aac1bd987f4b6bc8d6ead9b25ecdd32d69
Jonah Spear <Jonahspear512@gmail.com>
2017-09-10 19:45:20 -0400

    got test cases working

--- hw1.v
+++ hw1.v
@@ -4,13 +4,24 @@ module demorgan
   input B,
   output nA,
   output nB,
-  output nAandnB
+  output nAandnB,
+  output nAornB,
+  output n_AorB,
+  output n_AandB
 );
 
   wire nA;
   wire nB;
+  wire AandB;
+  wire AorB;
   not Ainv(nA, A);
   not Binv(nB, B);
   and andgate(nAandnB, nA, nB);
+  or orgate(nAornB, nA, nB);
 
+  and _AandB(AandB, A, B);
+  not _n_AandB(n_AandB, AandB);
+
+  or _AorB(AorB, A, B);
+  not _n_AorB(n_AorB, AorB);
 endmodule
