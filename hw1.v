a7fe7b0c8f7962de621666c16147c4ed6908d16e
Jonah Spear <Jonahspear512@gmail.com>
2017-09-10 18:41:37 -0400

    add gitignore, failing demorgan test

---
+++ hw1.v
@@ -0,0 +1,16 @@
+module demorgan
+(
+  input A,
+  input B,
+  output nA,
+  output nB,
+  output nAandnB
+);
+
+  wire nA;
+  wire nB;
+  not Ainv(nA, A);
+  not Binv(nB, B);
+  and andgate(nAandnB, nA, nB);
+
+endmodule
